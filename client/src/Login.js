e5dff5e566ea9647972c1e62796d6ed6ebe9de69
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-06-27 14:36:27 +1000

    complete react-router-challenge-one

---
+++ client/src/Login.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import styles from './styles.module.css';
+
+export default class Login extends Component {
+  state = { signup: false, email: '', password: '', name: '', token: null };
+
+  toggleSignup = () => {
+    this.setState(state => ({ signup: !state.signup }));
+  }
+
+  onNameChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  onEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  onPasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  signUp() {
+    const { name, email, password } = this.state;
+    return axios.post(
+      '/users',
+      { name, email, password }
+    ).then(() => this.login());
+  }
+
+  login() {
+    const { email, password } = this.state;
+    return axios.post(
+      '/login',
+      { email, password }
+    ).then(res => {
+      this.setState({
+        token: res.data.token,
+      });
+    });
+  }
+
+  onSubmit = () => {
+    if (this.state.signup) {
+      this.signUp();
+    } else {
+      this.login();
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <h1>Tanda Social Network</h1>
+
+        {this.state.signup && (
+          <input
+            className={styles.input}
+            onChange={this.onNameChange}
+            placeholder="Name"
+            type="text"
+            value={this.state.name}
+          />
+        )}
+        <input
+          className={styles.input}
+          onChange={this.onEmailChange}
+          placeholder="Email"
+          type="email"
+          value={this.state.email}
+        />
+        <input
+          className={styles.input}
+          onChange={this.onPasswordChange}
+          placeholder="Password"
+          type="password"
+          value={this.state.password}
+        />
+        <button
+          className={styles.button}
+          onClick={this.toggleSignup}
+        >
+          {this.state.signup ? 'I already have an account' : 'I need to sign up'}
+        </button>
+        <button
+          onClick={this.onSubmit}
+          className={styles.button}
+        >
+          Submit
+        </button>
+
+        {this.state.token && <p>You're logged in!</p>}
+      </div>
+    );
+  }
+}
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,113 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import axios from 'axios';
 import registerServiceWorker from './registerServiceWorker';
 import styles from './styles.module.css';
+import Login from './Login';
 
 axios.defaults.baseURL = 'http://social.workshops.tanda.co';
-class Login extends Component {
-  state = { signup: false, email: '', password: '', name: '', token: null };
-
-  toggleSignup = () => {
-    this.setState(state => ({ signup: !state.signup }));
-  }
-
-  onNameChange = (e) => {
-    this.setState({
-      name: e.target.value,
-    });
-  }
-
-  onEmailChange = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
-  }
-
-  onPasswordChange = (e) => {
-    this.setState({
-      password: e.target.value,
-    });
-  }
-
-  signUp() {
-    const { name, email, password } = this.state;
-    return axios.post(
-      '/users',
-      { name, email, password }
-    ).then(() => this.login());
-  }
-
-  login() {
-    const { email, password } = this.state;
-    return axios.post(
-      '/login',
-      { email, password }
-    ).then(res => {
-      this.setState({
-        token: res.data.token,
-      });
-    });
-  }
-
-  onSubmit = () => {
-    if (this.state.signup) {
-      this.signUp();
-    } else {
-      this.login();
-    }
-  }
-
-  render() {
-    return (
-      <div className={styles.wrapper}>
-        <h1>Tanda Social Network</h1>
-
-        {this.state.signup && (
-          <input
-            className={styles.input}
-            onChange={this.onNameChange}
-            placeholder="Name"
-            type="text"
-            value={this.state.name}
-          />
-        )}
-        <input
-          className={styles.input}
-          onChange={this.onEmailChange}
-          placeholder="Email"
-          type="email"
-          value={this.state.email}
-        />
-        <input
-          className={styles.input}
-          onChange={this.onPasswordChange}
-          placeholder="Password"
-          type="password"
-          value={this.state.password}
-        />
-        <button
-          className={styles.button}
-          onClick={this.toggleSignup}
-        >
-          {this.state.signup ? 'I already have an account' : 'I need to sign up'}
-        </button>
-        <button
-          onClick={this.onSubmit}
-          className={styles.button}
-        >
-          Submit
-        </button>
-
-        {this.state.token && <p>You're logged in!</p>}
-      </div>
-    );
-  }
-}
-
 ReactDOM.render(
   <div className={styles.app}>
-    <Login />
+    <BrowserRouter>
+      <div>
+        <Route exact path="/">
+          {() => <Redirect from="/" to="/login" />}
+        </Route>
+        <Route path="/login">
+          {() => <Login />}
+        </Route>
+      </div>
+    </BrowserRouter>
   </div>,
   document.getElementById('root')
 );
